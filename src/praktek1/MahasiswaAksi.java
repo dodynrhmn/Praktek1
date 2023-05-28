2b5c306a633b534101bc69c9955458e22cdffaf8
Dody Nurrahman <dodynrhmn@gmail.com>
2023-05-28 20:38:43 +0800

    Membuat Class Aksi dan Method main

---
+++ src/praktek1/MahasiswaAksi.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktek1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MahasiswaAksi {
+    public static void main(String[] args) {
+        
+    }
+}
